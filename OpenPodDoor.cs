e006c9bdbd9e452d117190f34f6d0fb836fe4c5f
Strigida <strigidacorvidae@gmail.com>
2018-12-12 14:30:09 -0700

    FinalUpdates

--- OpenPodDoor.cs
+++ OpenPodDoor.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
 
 public class OpenPodDoor : MonoBehaviour {
+
+    public AudioClip m_doorSound;
+    AudioSource audioSource;
 
 	public Animator m_doorAnimator;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,6 +27,7 @@ public class OpenPodDoor : MonoBehaviour {
         {
             m_doorAnimator.SetTrigger("openPodDoorHal");
             GetComponent<MeshRenderer>().material.color = new Color32(255, 248, 175, 255);
+            audioSource.PlayOneShot(m_doorSound, 1f);
         }
 	}
 }
---
+++ TreeNoises.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class TreeNoises : MonoBehaviour {
+
+    public AudioClip m_treeImpactSound;
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "hand")
+        {
+            audioSource.PlayOneShot(m_treeImpactSound, 1f);
+        }
+    }
+}
